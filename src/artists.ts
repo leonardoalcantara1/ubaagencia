b85b4eeff311406338e3896649301ac12a696874
Leonardo Martinez de Alcântara <leonardoalcantara001@gmail.com>
2024-05-02 21:36:40 -0300

    update player emize

--- src/artists.ts
+++ src/artists.ts
@@ -17,16 +17,15 @@ const artists: IArtists = {
   emize: {
     presskit: '/downloads/Emizê - Presskit 2024.zip',
     release:
-      'Leonardo Martinez traz seu projeto de progressive trance e hardprog chamado EMIZÊ, com um set super energético e dançante. Cantor, musicista, multi instrumentista, Leo passou a vida estudando e vivendo a música. Natural de São Paulo, usa toda a sua expertise musical e profissional trilhando o caminho da produção dentro da cena, remixando e produzindo seu set com muita identidade. Hoje idealizador da Ubá Cultura Trance, também faz parte de projetos como Onda Norte e Ubah Festival.',
+      'Leonardo Martinez traz seu projeto de progressive trance chamado EMIZÊ, com um set elegante, melódico e dançante. Cantor, musicista, multi instrumentista, Leo passou a vida estudando e vivendo a música. Natural de São Paulo, usa toda a sua expertise musical e profissional trilhando o caminho da produção dentro da cena, remixando e produzindo seu set com muita identidade. Hoje idealizador do coletivo da UBÁ, também faz parte de vários projetos no litoral extremo norte de São Paulo.',
     player: [
-      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1774765290&color=%238cb4b4&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
-      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1774757667&color=%238cb4b4&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true',
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1813071453&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
     ],
     soundcloud: 'leoemize',
     instagram: 'leo.emize',
-    title: 'EMIZÊ - Projeto de Progressive trance e HardProg :: Ubá Coletivo',
+    title: 'EMIZÊ - Projeto de Progressive trance :: Ubá Coletivo',
     description:
-      'Emizê é o projeto de progressive trance e hardprog de Leonardo Martinez, DJ e produtor musical fundador do coletivo de artistas da UBÁ.',
+      'Emizê é o projeto de progressive trance de Leonardo Martinez, DJ e produtor musical fundador do coletivo de artistas da UBÁ.',
   },
   comicbass: {
     presskit: '/downloads/ComicBass - Presskit 2024.zip',
